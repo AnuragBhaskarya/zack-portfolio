e30b7cab83c3bf1ac94936b2aae3bad1bdf220ab
so9ic <anuragbhaskarya@gmail.com>
2026-09-04 23:48:02 +0530

    Reduce Order Now animation delay

--- script.js
+++ script.js
@@ -505,7 +505,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (text) text.classList.add('animate');
             }, 150); 
         }
-    }, 1500); 
+    }, 1600); 
 
     // --- FAQ Banner Animations ---
     const faqBannerContainer = document.querySelector('.faq-banner-container');
